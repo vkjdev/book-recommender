5aeec2f52d4369e13598e5880b3bc82403940801
matejkvassay <matejkvassay5@gmail.com>
2016-11-23 11:36:20 +0100

    NoneType rating expected in evaluator.

--- recommender/evaluator.py
+++ recommender/evaluator.py
@@ -7,12 +7,12 @@ import logging
 import math
 import pandas as pd
 import numpy as np
-#from dummy_recommender import MeanRatingRecommender as Recommender
-#from knn_recommender_v2 import KnnRecommender as Recommender
+# from dummy_recommender import MeanRatingRecommender as Recommender
+# from knn_recommender_v2 import KnnRecommender as Recommender
 from doc2vec_recommender import Doc2VecRecommender as Recommender
 
-#DATA_FILE_PATH="/home/michal/Documents/Misc/recommenders/vcs/book-recommender/data/ratings_Books.csv"
-DATA_FILE_PATH='/home/kvassay/data/book-recommender/ratings_Books_100K.csv'
+# DATA_FILE_PATH="/home/michal/Documents/Misc/recommenders/vcs/book-recommender/data/ratings_Books.csv"
+DATA_FILE_PATH = '/home/kvassay/data/book-recommender/ratings_Books_100K.csv'
 
 SAMPLED_USERS = 1000
 USER_IS_ROBOT_THRESHOLD = 1000
@@ -46,10 +46,9 @@ logger.info("Starting application on %s dataset" % data_file)
 logger.info("Testing recommender implementation of %s " % method_name)
 
 with open(data_file, "r") as f:
-
     df = pd.read_csv(data_file, names=["user", "item", "rating", "timestamp"])
 
-    logger.info('First row of csv: %s',df.head(1))
+    logger.info('First row of csv: %s', df.head(1))
     grouped_users = df.groupby(["user"]).count()
     n_review_users = grouped_users[grouped_users["item"] >= SLICING_INTERVAL]["item"].keys()
     eval_users = np.random.choice(n_review_users.unique(), SAMPLED_USERS)
@@ -59,14 +58,14 @@ with open(data_file, "r") as f:
     logger.info("Selected dataframe of random %s users containing %s entries" %
                 (eval_users.__len__(), eval_dataframe.__len__()))
 
-    for eval_run in range(SLICING_INTERVAL-SLICING_RUNS, SLICING_INTERVAL):
+    for eval_run in range(SLICING_INTERVAL - SLICING_RUNS, SLICING_INTERVAL):
         # each test run starts with clean recommender instance
         tested_recommender = Recommender()
 
         training_frame = pd.DataFrame(columns=["user", "item", "rating", "timestamp"])
         testing_frame = pd.DataFrame(columns=["user", "item", "rating", "timestamp"])
 
-        quantile = eval_run*(1/float(SLICING_INTERVAL))
+        quantile = eval_run * (1 / float(SLICING_INTERVAL))
         logger.info("training on users dataset divides on quantile %s" % quantile)
 
         # TODO: later compare recommender results with using dataset having only data newer than from 2013
@@ -97,6 +96,7 @@ with open(data_file, "r") as f:
 
         # aggregated difference of recommender predicted rating against the real rating
         delta_sum = 0
+        len_diff = 0
         for index, entry in testing_frame.iterrows():
             expected_score = entry["rating"]
             actual_score = tested_recommender.predict(entry["user"], entry["item"])
@@ -104,11 +104,14 @@ with open(data_file, "r") as f:
             # TODO: remove for no output of matching
             logger.info("expected - actual: %s - %s" % (expected_score, actual_score))
 
-            delta_sum += math.fabs(expected_score-actual_score)
+            if actual_score is not None:
+                delta_sum += math.fabs(expected_score - actual_score)
+            else:
+                len_diff += 1
 
         logger.info("Recommender method has predicted %s ratings" % testing_frame.__len__())
 
-        delta = float(delta_sum)/testing_frame.__len__()
+        delta = float(delta_sum) / (testing_frame.__len__() - len_diff)
         mean_rating = float(testing_frame["rating"].mean())
 
         logger.info("Testing data mean rating: %s" % mean_rating)
